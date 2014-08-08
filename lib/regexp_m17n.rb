502ed829242ae6bce73a837cd8a6b2b5f5672ff3
Gordon Gao <ghprince@gmail.com>
2014-08-08 13:53:57 +0800

    Fallback dummy encoding to utf-8

--- lib/regexp_m17n.rb
+++ lib/regexp_m17n.rb
@@ -1,5 +1,6 @@
 module RegexpM17N
   def self.non_empty?(str)
-    str =~ /^.+$/
+    str = str.force_encoding('utf-8') if str.encoding.dummy?
+    str =~ Regexp.new("^.+$".encode(str.encoding))
   end
 end
--- test/regexp_m17n_test.rb
+++ test/regexp_m17n_test.rb
@@ -5,7 +5,11 @@ require_relative '../lib/regexp_m17n'
 class RegexpTest < MiniTest::Unit::TestCase
   def test_non_empty_string
     Encoding.list.each do |enc|
-      assert(RegexpM17N.non_empty?('.'.encode(enc)))
+      if enc.dummy?
+        assert(RegexpM17N.non_empty?('.'.force_encoding(enc)))
+      else
+        assert(RegexpM17N.non_empty?('.'.encode(enc)))
+      end
     end
   end
 end
